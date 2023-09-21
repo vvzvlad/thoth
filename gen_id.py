89c7eb29e17529a5408a9ff2856ff025fb6d0f9d
vvzvlad <git@vvzvlad.xyz>
2023-09-21 14:56:52 +0300

    update

--- gen_id.py
+++ gen_id.py
@@ -3,6 +3,18 @@ import hashlib
 import os
 import base64
 import sys
+from datetime import datetime
+import pytz
+
+def convert_to_unix_time(timestamp):
+    try:
+        dt = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f%z")
+        dt = dt.astimezone(pytz.UTC)
+        unix_time = int(dt.timestamp())
+        return unix_time
+    except Exception as e:
+        print(f"Could not convert timestamp: {e}")
+        return None
 
 def generate_hash(data, timestamp):
     m = hashlib.sha256()
@@ -12,6 +24,7 @@ def generate_hash(data, timestamp):
     return base64.b64encode(hash_value).decode('utf-8').replace('+', '-').replace('/', '_').strip('=')
 
 def process_file(file_path):
+
     try:
         with open(file_path, 'r') as f:
             data = json.load(f)
@@ -19,25 +32,35 @@ def process_file(file_path):
         print(f"Skipping {file_path}. Invalid or empty JSON.")
         return
 
-    if 'id' in data:
-        return
+    if 'id' not in data:
+        data['id'] = generate_hash(data['page_text'], data['timestamp'])[:10]
+
+    print(f"Processing {file_path}. Assigned ID: {data['id']}, {data['timestamp']}")
 
     timestamp = data.get('timestamp', '')
-    data_str = json.dumps(data, sort_keys=True)
-
-    new_id = generate_hash(data_str, timestamp)[:10]
-    data['id'] = new_id
-
-    print(f"Processing {file_path}. Assigned ID: {new_id}")
+    data['timestamp'] = convert_to_unix_time(timestamp)
 
     with open(file_path, 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(data, f, indent=4, sort_keys=True)
 
 def process_directory(directory):
+    total_files = 0
+    processed_files = 0
+
+    # Count total JSON files
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json'):
+                total_files += 1
+
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.json'):
                 process_file(os.path.join(root, file))
+                processed_files += 1
+                percentage_done = (processed_files / total_files) * 100
+                print(f"Processed: {processed_files}/{total_files} ({percentage_done:.2f}%)")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
--- meilisearch_upload.py
+++ meilisearch_upload.py
@@ -8,14 +8,27 @@ def send_file_to_meilisearch(file_path, url, auth_token):
         'Content-Type': 'application/json'
     }
 
+    status_messages = {
+        200: "✅ Ok: Data sent successfully",
+        201: "✅ Created: Resource has been created",
+        202: "✅ Accepted: Task added to queue",
+        204: "✅ No Content: Resource deleted or no content returned",
+        205: "✅ Reset Content: All resources have been deleted",
+        400: "❌ Bad Request: The request was unacceptable",
+        401: "❌ Unauthorized: No valid API key provided",
+        403: "❌ Forbidden: API key lacks permissions",
+        404: "❌ Not Found: The requested resource doesn't exist"
+    }
+
     with open(file_path, 'rb') as f:
         data = f.read()
 
     response = requests.post(url, headers=headers, data=data)
-    if response.status_code == 200:
-        print(f"Successfully sent {file_path} to Meilisearch")
-    else:
-        print(f"Failed to send {file_path} to Meilisearch. Status code: {response.status_code}")
+    status_code = response.status_code
+    message = status_messages.get(status_code, f"❌ Unknown Status Code: {status_code}")
+
+    print(f"{file_path} - {message}")
+
 
 def main(args):
     for root, _, files in os.walk(args.directory):
