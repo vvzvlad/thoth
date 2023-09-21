c4d5256c83506287f9f56c6cbb53c32becc4e820
vvzvlad <git@vvzvlad.xyz>
2023-09-21 12:30:04 +0300

    refactoring

--- thoth-server.py
+++ thoth-server.py
@@ -7,19 +7,18 @@
 #############################################################################
 #
 
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 import json
 from datetime import datetime
 import os
-import os.path
 import hashlib
 import requests
-import os
 import base64
 
-MEILI_SERVER_URL = os.environ.get('MEILI_SERVER_URL', 'http://10.31.41.59:7700/indexes/thoth/documents')
-MEILI_AUTH_TOKEN = os.environ.get('MEILI_AUTH_TOKEN', 'Hurt3-Ointment-Gestate')
+MEILI_SERVER_URL = os.environ.get('MEILI_SERVER_URL')
+MEILI_AUTH_TOKEN = os.environ.get('MEILI_AUTH_TOKEN')
 
 
 def generate_hash(data, timestamp):
@@ -31,12 +30,9 @@ def generate_hash(data, timestamp):
 
 
 def send_to_meilisearch_server(json_data, url, auth_token):
-    headers = {
-        'Authorization': f'Bearer {auth_token}',
-        'Content-Type': 'application/json'
-    }
+    headers = { 'Authorization': f'Bearer {auth_token}', 'Content-Type': 'application/json' }
     try:
-        response = requests.post(url, headers=headers, data=json_data)
+        response = requests.post(url, headers=headers, data=json_data, timeout=10)
         if response.status_code == 200:
             print(f"Data sent successfully to {url}")
         else:
@@ -45,89 +41,77 @@ def send_to_meilisearch_server(json_data, url, auth_token):
         print(f"An error occurred while sending data to {url}: {e}")
 
 
-
 def parse_and_write_file(json_data):
     try:
-      data = json.loads(json_data)
+        data = json.loads(json_data)
     except:
-      print("Failed to parse JSON")
-      return {"error":True,"message":"Invalid JSON"}
+        print("Failed to parse JSON")
+        return {"error": True, "message": "Invalid JSON"}
 
     try:
-      timestamp = datetime.fromisoformat(data['timestamp'])
+        timestamp = datetime.fromisoformat(data['timestamp'])
     except:
-      print("Failed to parse timestamp")
-      return {"error":True,"message":"Invalid timestamp, must be ISO8601: YYYY-MM-DDTHH:MM:SS.mmmmmm"}
+        print("Failed to parse timestamp")
+        return {"error": True, "message": "Invalid timestamp, must be ISO8601: YYYY-MM-DDTHH:MM:SS.mmmmmm"}
 
     try:
-      site = data['url'].split('/')[2]
-      filename_hash = hashlib.md5(data['url'].encode('utf-8')).hexdigest()
-
-      filename = "{hour}_{minute}_{second}_{site}_{filename_hash}.json".format(site=site, timestamp=data['timestamp'], filename_hash=filename_hash, hour=timestamp.hour, minute=timestamp.minute, second=timestamp.second)
-      path = "/data/raw_data/{year}/{month}/{day}/".format(year=timestamp.year, month=timestamp.month, day=timestamp.day)
+        site = data['url'].split('/')[2]
+        filename_hash = hashlib.md5(data['url'].encode('utf-8')).hexdigest()
+        filename = f"{timestamp.hour}_{timestamp.minute}_{timestamp.second}_{site}_{filename_hash}.json"
+        path = f"/data/raw_data/{timestamp.year}/{timestamp.month}/{timestamp.day}/"
     except Exception as e:
-      print("Internal Server Error")
-      return {"error":True,"message":"Internal Server Error: {}".format(e.message)}
+        print("Internal Server Error")
+        return {"error": True, "message": f"Internal Server Error: {e}"}
 
     if 'id' not in data:
-        timestamp = data.get('timestamp', '')
+        print(f"Generate id: {data['id']}")
+        timestamp_str = data.get('timestamp', '')
         data_str = json.dumps(data, sort_keys=True)
-        new_id = generate_hash(data_str, timestamp)[:10]
+        new_id = generate_hash(data_str, timestamp_str)
         data['id'] = new_id
 
     send_to_meilisearch_server(json_data, MEILI_SERVER_URL, MEILI_AUTH_TOKEN)
 
-
     if not os.path.exists(path):
-      try:
-        os.makedirs(path)
-      except :
-        print("Failed to create directory")
-        return {"error":True,"message":"Not create directory: {path}".format(path=path)}
+        try:
+            os.makedirs(path)
+        except:
+            print("Failed to create directory")
+            return {"error": True, "message": f"Not create directory: {path}"}
 
-    if not os.path.exists(path+filename):
-      try:
-        with open(path+filename, 'wb') as file:
-            data_len = file.write(json_data)
-            #data_len = file.write(json.dumps(data, indent=4, sort_keys=True).encode('utf-8'))
-      except Exception as e:
-        print("Failed to write file: {}".format(e))
-        return {"error":True,"message":"Not write file: {path}{filename}".format(path=path, filename=filename)}
-      print("Data saved: page name: \"{name}\", url: {url}, filename: {filename}, {len} kbytes, time: {timestamp} ".format(len=data_len/1000, url=data['url'], name=data['page_name'], timestamp=data['timestamp'], filename=filename))
-      return {"error":False,"message":"Data saved"}
+    if not os.path.exists(f"{path}{filename}"):
+        try:
+            with open(f"{path}{filename}", 'wb') as file:
+                data_len = file.write(json_data)
+        except Exception as e:
+            print(f"Failed to write file: {e}")
+            return {"error": True, "message": f"Not write file: {path}{filename}"}
+        print(f"Data saved: page name: {data['page_name']}, url: {data['url']}, filename: {filename}, {data_len/1000} kbytes, time: {data['timestamp']}")
+        return {"error": False, "message": "Data saved"}
     else:
-      print("File already exists: \"{name}\", url: {url}, filename: {filename}, time: {timestamp} ".format(url=data['url'], name=data['page_name'], timestamp=data['timestamp'], filename=filename))
-      return {"error":True,"message":"File already exists: {path}{filename}".format(path=path, filename=filename)}
-
-
+        print(f"File already exists: {data['page_name']}, url: {data['url']}, filename: {filename}, time: {data['timestamp']}")
+        return {"error": True, "message": f"File already exists: {path}{filename}"}
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        parsed_path = urlparse(self.path)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(json.dumps({
-            'error': True,
-            'message': "Use POST method",
-        }).encode())
+        self.wfile.write(json.dumps({'error': True, 'message': "Use POST method"}).encode())
         return
 
     def do_POST(self):
         content_len = int(self.headers.get('content-length'))
         post_body = self.rfile.read(content_len)
         ret = parse_and_write_file(post_body)
-
-        parsed_path = urlparse(self.path)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(json.dumps({
-            'error': ret["error"],
-            'message': ret["message"],
-        }).encode())
+        self.wfile.write(json.dumps({'error': ret["error"], 'message': ret["message"]}).encode())
         return
+
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 8000), RequestHandler)
     print('Starting server at http://localhost:8000')
+    print(f"Meilisearch instance: {MEILI_SERVER_URL}, token: {MEILI_AUTH_TOKEN}")
     server.serve_forever()
